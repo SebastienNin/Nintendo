e3771c6f263bd98be6c7249f1885ba21fc5e14f8
thrmed <tahirmedjadba@gmail.com>
2023-07-27 10:04:20 +0200

    Modif classe Console ajouts attribut + constr + get et set

--- NintendoProject/NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/NintendoProject/src/main/java/metier/Console.java
@@ -1,11 +1,20 @@
 package metier;
 
+import java.time.LocalDate;
+
 public abstract class Console {
 
-	private String nom;
+	protected String nom;
+	protected Integer prix;
+	protected LocalDate dateSortie;
 
-	public Console(String nom) {
+	
+
+	public Console(String nom, Integer prix, LocalDate dateSortie) {
+		super();
 		this.nom = nom;
+		this.prix = prix;
+		this.dateSortie = dateSortie;
 	}
 
 	public String getNom() {
@@ -15,11 +24,29 @@ public abstract class Console {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	public Integer getPrix() {
+		return prix;
+	}
+
+	public void setPrix(Integer prix) {
+		this.prix = prix;
+	}
+
+	public LocalDate getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(LocalDate dateSortie) {
+		this.dateSortie = dateSortie;
+	}
 
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", dateSortie=" + dateSortie + "]";
 	}
+
+
 	
 	
 }
