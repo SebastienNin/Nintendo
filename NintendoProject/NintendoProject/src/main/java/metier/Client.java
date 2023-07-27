0e4b30d283d6040b8d89db57ea4e4fb13b0085c1
Tidiane23 <tidianediallo426@gmail.com>
2023-07-27 10:29:37 +0200

    Faire evoluer la classe client en y ajoutant une liste d'achat

--- NintendoProject/NintendoProject/src/main/java/metier/Client.java
+++ NintendoProject/NintendoProject/src/main/java/metier/Client.java
@@ -1,14 +1,24 @@
 package metier;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
 	private String nom;
 	private String prenom;
+	private List<Achat> achats = new ArrayList<>();;
+	
 	
 	public Client(String nom, String prenom) {
-		super();
 		this.nom = nom;
 		this.prenom = prenom;
 	}
+	
+	/*public void ajouterAchat(Jeu jeu, LocalDate date, int prix) {
+		Achat nouvelAchat = new Achat(jeu, date, prix);
+		achats.add(nouvelAchat);
+	}*/
 
 	public String getNom() {
 		return nom;
@@ -26,11 +36,18 @@ public class Client {
 		this.prenom = prenom;
 	}
 
+	public List<Achat> getAchats() {
+		return achats;
+	}
+
+	public void setAchats(List<Achat> achats) {
+		this.achats = achats;
+	}
+
 	@Override
 	public String toString() {
-		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
-	}
-	
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", achats=" + achats + "]";
+	}	
 	
 	
 }
